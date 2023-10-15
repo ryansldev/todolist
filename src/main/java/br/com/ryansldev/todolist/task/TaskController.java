b89e9d40142867d2d9b13aaf7c257e9e14afd782
ryansldev <ryanlima1309@gmail.com>
2023-10-15 16:36:05 -0300

    feat: add task update

--- src/main/java/br/com/ryansldev/todolist/task/TaskController.java
+++ src/main/java/br/com/ryansldev/todolist/task/TaskController.java
@@ -7,11 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.ryansldev.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -44,6 +47,18 @@ public class TaskController {
     var tasks = this.taskRepository.findByIdUser((UUID) idUser);
 
     return ResponseEntity.status(HttpStatus.OK).body(tasks);
+  }
+
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {   
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    System.out.println(taskModel);
+    System.out.println("TASK " + task);
+
+    Utils.copyNonNullProperties(taskModel, task);
+    
+    return this.taskRepository.save(task);
   }
 
 }
