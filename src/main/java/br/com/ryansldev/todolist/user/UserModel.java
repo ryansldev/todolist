467fe554432c4d554f8c5c8f5d4d07f186043729
ryansldev <ryanlima1309@gmail.com>
2023-10-15 08:49:28 -0300

    feat: add lombok

--- src/main/java/br/com/ryansldev/todolist/user/UserModel.java
+++ src/main/java/br/com/ryansldev/todolist/user/UserModel.java
@@ -1,7 +1,12 @@
 package br.com.ryansldev.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
+
   public String username;
   public String name;
   public String password;
+  
 }
