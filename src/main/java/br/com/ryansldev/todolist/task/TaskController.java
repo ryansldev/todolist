a45153ee82cad18e06b8cdbff96c3a8f20beb568
ryansldev <ryanlima1309@gmail.com>
2023-10-15 16:06:17 -0300

    feat: add list

--- src/main/java/br/com/ryansldev/todolist/task/TaskController.java
+++ src/main/java/br/com/ryansldev/todolist/task/TaskController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,14 @@ public class TaskController {
 
     var task = this.taskRepository.save(taskModel);
     return ResponseEntity.status(HttpStatus.OK).body(task);
+  }
+
+  @GetMapping("/")
+  public ResponseEntity list(HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+    var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+
+    return ResponseEntity.status(HttpStatus.OK).body(tasks);
   }
 
 }
