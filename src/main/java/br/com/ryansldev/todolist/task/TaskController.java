e86c4e2c04d03380870a1227fb35634ba2e9d640
ryansldev <ryanlima1309@gmail.com>
2023-10-15 17:13:43 -0300

    feat: add error handler

--- src/main/java/br/com/ryansldev/todolist/task/TaskController.java
+++ src/main/java/br/com/ryansldev/todolist/task/TaskController.java
@@ -50,15 +50,19 @@ public class TaskController {
   }
 
   @PutMapping("/{id}")
-  public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {   
+  public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {   
     var task = this.taskRepository.findById(id).orElse(null);
 
-    System.out.println(taskModel);
-    System.out.println("TASK " + task);
+    var idUser = request.getAttribute("idUser");
+
+    if(!task.getIdUser().equals(idUser)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissão para alterar essa tarefa");
+    }
 
     Utils.copyNonNullProperties(taskModel, task);
-    
-    return this.taskRepository.save(task);
+    var taskUpdated = this.taskRepository.save(task);
+
+    return ResponseEntity.ok().body(taskUpdated);
   }
 
 }
